7c38b6926e3dabd76fa4cfda178feea2c324d8bc
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2021-05-05 16:48:17 +0300

    Add debug logs

--- app/confirmation.lua
+++ app/confirmation.lua
@@ -1,7 +1,12 @@
+local log = require('log')
+
 local confirmation_token = os.getenv('VK_BOT_CONFIRMATION')
 
 local function handler(_)
+    log.verbose('Process confirmation request')
+
     if confirmation_token == nil then
+        log.error('VK_BOT_CONFIRMATION is not set, can not respond to confirmation request')
         return {
             status = 500,
             body = 'VK_BOT_CONFIRMATION is not set'
