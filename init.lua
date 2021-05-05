cfdea1dc1c3103b6c70c86e2e77b5cab3d1b0aa0
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2021-05-05 15:01:43 +0300

    Set confirmation string with env

--- init.lua
+++ init.lua
@@ -1,15 +1,20 @@
 #!/usr/bin/env tarantool
 
-local confirmation_string = require('app.confirmation_string')
-
-local host_port = os.getenv("PORT") or 8081
+local host_port = os.getenv('PORT') or 8081
+local confirmation_token = os.getenv('VK_BOT_CONFIRMATION')
 
 local function api_handler(request)
     local body = request:json()
     if body.type == 'confirmation' then
+        if confirmation_token == nil then
+            return {
+                status = 500,
+                body = 'VK_BOT_CONFIRMATION not set'
+            }
+        end
         return {
             status = 200,
-            body = confirmation_string[body.group_id],
+            body = confirmation_token,
         }
     else
         return {
